31ff55bc22b9a282f73197de672dde80aa89f63d
Kevin Shandler Silva do Carmo Santos <82344661+KevinShandler@users.noreply.github.com>
2022-08-16 22:22:09 -0300

    manipular_lista_2.0
    Agora a lista vai apenas com item teste.
    O menu está mais bonito.
    A opção 1 agora tem uma pequena confirmação se a lista possui algum item a ser listado.
    Foi adicionado um ELSE para tratar opções invalidas.

--- manipular_lista.py
+++ manipular_lista.py
@@ -1,28 +1,63 @@
-# imprimir uma etiqueta
-
-produtos = ['FACA', 'COLHER', 'GARFO', 'FEIJÃO', 'ARROZ', 'AÇUCAR']
+# acabou virando uma lista :)
+produtos = ['TESTE']
 produto = ''
 sair = 0
-escolha = int(input('Escolha uma opção: \n1-Listar Produtos\n2-Cadastrar Produto'
-                    '\n3-Remover Produto \n4-Sair\n'))
+escolha = int(input('----------Menu----------\n'
+                    '1 - Listar Produtos\n'
+                    '2 - Cadastrar Produto\n'
+                    '3 - Remover Produto\n'
+                    '4 - Sair\n'
+                    '------------------------\n'))
 
-while sair <= 1:
+while sair < 1:
     if escolha == 1:
-        print('\n')
-        print(produtos)
-        print('\n')
-        escolha = int(input('Escolha uma opção: \n1-Listar Produtos\n2-Cadastrar Produto'
-                            '\n3-Remover Produto \n4-Sair\n'))
+        if not produtos:
+            print('\nNenhum item cadastrado\n')
+            escolha = int(input('----------Menu----------\n'
+                                '1 - Listar Produtos\n'
+                                '2 - Cadastrar Produto\n'
+                                '3 - Remover Produto\n'
+                                '4 - Sair\n'
+                                '------------------------\n'))
+        else:
+            print('\n')
+            print(produtos)
+            print('Total de itens: ', len(produtos))
+            print('\n')
+            escolha = int(input('----------Menu----------\n'
+                                '1 - Listar Produtos\n'
+                                '2 - Cadastrar Produto\n'
+                                '3 - Remover Produto\n'
+                                '4 - Sair\n'
+                                '------------------------\n'))
     elif escolha == 2:
         produto = str(input('Nome do produto: ', ))
         produtos.append(produto.upper())
-        print('Produto cadastrado com sucesso.')
-        escolha = int(input('Escolha uma opção: \n1-Listar Produtos\n2-Cadastrar Produto'
-                            '\n3-Remover Produto \n4-Sair\n'))
+        print('Produto cadastrado com sucesso.\n')
+        escolha = int(input('----------Menu----------\n'
+                            '1 - Listar Produtos\n'
+                            '2 - Cadastrar Produto\n'
+                            '3 - Remover Produto\n'
+                            '4 - Sair\n'
+                            '------------------------\n'))
     elif escolha == 3:
         produto = input('Qual produto deseja remover: ')
         produtos.remove(produto.upper())
-        escolha = int(input('Escolha uma opção: \n1-Listar Produtos\n2-Cadastrar Produto'
-                            '\n3-Remover Produto \n4-Sair\n'))
+        print('\nItem removido com sucesso\n')
+        escolha = int(input('----------Menu----------\n'
+                            '1 - Listar Produtos\n'
+                            '2 - Cadastrar Produto\n'
+                            '3 - Remover Produto\n'
+                            '4 - Sair\n'
+                            '------------------------\n'))
     elif escolha == 4:
         sair = 2
+
+    else:
+        print('\nOpção invalida\n')
+        escolha = int(input('----------Menu----------\n'
+                            '1 - Listar Produtos\n'
+                            '2 - Cadastrar Produto\n'
+                            '3 - Remover Produto\n'
+                            '4 - Sair\n'
+                            '------------------------\n'))
