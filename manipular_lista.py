85d5b33bd4a0f186fd10e5931c37aef1d66d29a1
Kevin Shandler Silva do Carmo Santos <82344661+KevinShandler@users.noreply.github.com>
2022-08-19 21:25:29 -0300

    manipular_lista_3.0

--- manipular_lista.py
+++ manipular_lista.py
@@ -2,62 +2,31 @@
 produtos = ['TESTE']
 produto = ''
 sair = 0
-escolha = int(input('----------Menu----------\n'
-                    '1 - Listar Produtos\n'
-                    '2 - Cadastrar Produto\n'
-                    '3 - Remover Produto\n'
-                    '4 - Sair\n'
-                    '------------------------\n'))
 
 while sair < 1:
+    escolha = int(input('----------Menu----------\n'
+                        '1 - Listar Produtos\n'
+                        '2 - Cadastrar Produto\n'
+                        '3 - Remover Produto\n'
+                        '4 - Sair\n'
+                        '------------------------'))
     if escolha == 1:
         if not produtos:
             print('\nNenhum item cadastrado\n')
-            escolha = int(input('----------Menu----------\n'
-                                '1 - Listar Produtos\n'
-                                '2 - Cadastrar Produto\n'
-                                '3 - Remover Produto\n'
-                                '4 - Sair\n'
-                                '------------------------\n'))
         else:
             print('\n')
             print(produtos)
             print('Total de itens: ', len(produtos))
             print('\n')
-            escolha = int(input('----------Menu----------\n'
-                                '1 - Listar Produtos\n'
-                                '2 - Cadastrar Produto\n'
-                                '3 - Remover Produto\n'
-                                '4 - Sair\n'
-                                '------------------------\n'))
     elif escolha == 2:
         produto = str(input('Nome do produto: ', ))
         produtos.append(produto.upper())
         print('Produto cadastrado com sucesso.\n')
-        escolha = int(input('----------Menu----------\n'
-                            '1 - Listar Produtos\n'
-                            '2 - Cadastrar Produto\n'
-                            '3 - Remover Produto\n'
-                            '4 - Sair\n'
-                            '------------------------\n'))
     elif escolha == 3:
         produto = input('Qual produto deseja remover: ')
         produtos.remove(produto.upper())
         print('\nItem removido com sucesso\n')
-        escolha = int(input('----------Menu----------\n'
-                            '1 - Listar Produtos\n'
-                            '2 - Cadastrar Produto\n'
-                            '3 - Remover Produto\n'
-                            '4 - Sair\n'
-                            '------------------------\n'))
     elif escolha == 4:
         sair = 2
-
     else:
         print('\nOpção invalida\n')
-        escolha = int(input('----------Menu----------\n'
-                            '1 - Listar Produtos\n'
-                            '2 - Cadastrar Produto\n'
-                            '3 - Remover Produto\n'
-                            '4 - Sair\n'
-                            '------------------------\n'))
