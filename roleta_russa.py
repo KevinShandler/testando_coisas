d92b6065d3f1fca0d2d79c973d067927971d8f00
Kevin Shandler Silva do Carmo Santos <82344661+KevinShandler@users.noreply.github.com>
2022-08-19 21:30:22 -0300

    Update roleta_russa.py

--- roleta_russa.py
+++ roleta_russa.py
@@ -1,14 +1,13 @@
 import random
 import time
 
-escolha = input('Voce deseja jogar ROLETA RUSSA? S/N: ')
 morte = 0
 bala = random.randint(1, 6)
 pontuacao = 0
 
 
 while morte == 0:
-
+    escolha = input('\nVoce deseja jogar ROLETA RUSSA? S/N: ')
     if escolha == 's' and 'S':
         print('Posição da bala:', bala)
         time.sleep(1)
@@ -19,9 +18,9 @@ while morte == 0:
             print('Falhou.')
             pontuacao = pontuacao + 1
         elif resultado == bala:
-            print('Voce morreu, XD.')
+            print('Voce morreu, XD.', '\nPontuação: ', pontuacao)
             morte = 1
-            print('Pontuação: ', pontuacao)
     elif escolha != 's' and 'S':
-        print('Jogo Encerrado.')
+        print('Jogo Encerrado.', '\nPontuação: ', pontuacao)
         morte = 1
+
